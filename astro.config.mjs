ba6b4378354c887dba98fbdce36e58eb3f2f19f9
Ion <Ion2000m@gmail.com>
2023-12-18 12:28:00 +0100

    Update Astro configuration and add CommonHead component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+import { loadEnv } from 'vite';
+
+const { IMAGE_DOMAIN } = loadEnv(process.env.NODE_ENV, process.cwd(), '');
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind({
-        applyBaseStyles: false,
-    })],
+    image: {
+        domains: [IMAGE_DOMAIN],
+    },
+    integrations: [
+        tailwind({
+            applyBaseStyles: false,
+        }),
+    ],
 });
