362dcaf61d7794abe69b6f3a5c35d1e72a0ee4db
Ion <Ion2000m@gmail.com>
2023-12-18 15:02:23 +0100

    Add JSX configuration and FontAwesome imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import { loadEnv } from 'vite';
+import react from "@astrojs/react";
+const {
+  IMAGE_DOMAIN
+} = loadEnv(process.env.NODE_ENV, process.cwd(), '');
 
-const { IMAGE_DOMAIN } = loadEnv(process.env.NODE_ENV, process.cwd(), '');
 
 // https://astro.build/config
 export default defineConfig({
-    image: {
-        domains: [IMAGE_DOMAIN],
-    },
-    integrations: [
-        tailwind({
-            applyBaseStyles: false,
-        }),
-    ],
+  image: {
+    domains: [IMAGE_DOMAIN]
+  },
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), react()]
 });
