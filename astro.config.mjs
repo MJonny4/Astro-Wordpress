c27691964324000532858dd00009eef9b5062616
Ion <Ion2000m@gmail.com>
2023-12-20 17:54:06 +0100

    Add @astrojs/vercel package and update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import { loadEnv } from 'vite';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
 const { PUBLIC_WP_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), '');
 
 // https://astro.build/config
@@ -15,4 +16,6 @@ export default defineConfig({
         }),
         react(),
     ],
+    output: 'hybrid',
+    adapter: vercel(),
 });
